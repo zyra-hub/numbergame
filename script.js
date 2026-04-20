c33529a23746d289183cd5c495efb3d06ae2c9c0
Jason Alakiri <codemiq@gmail.com>
2026-04-20 07:38:28 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+// /need a secret Number
+// ask the user for a guess
+// compare with my secret num
+// reapat cond until its correct/ psudo code
+
+let secret = Math.floor(Math.random() * 20) + 1;
+// guess
+let guess = prompt("Guess a number between 1 and 20");
+// compare
+// guess == secret
+while (guess != secret) {
+    if (guess > secret) {
+        alert("Too HIGH");
+    } else {
+        alert("Too Low!");
+    }
+    guess = prompt("Try Again");
+}
+alert("CORRECT!!!");
+// while (i > guess) {
+//     prompt("Guess a number between 1 and 20")
+// }
